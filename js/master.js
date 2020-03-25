ba7a0dcd72dd4b506e9b5a4893b5d89bc542d122
Bjorn Meurkes <bjorn.meurkes@gmail.com>
2020-03-25 18:04:29 +0100

    supplier en index gelinkt

--- js/master.js
+++ js/master.js
@@ -1,3 +1,15 @@
 window.onload= function() {
+  supplier1card = document.getElementById('supplier1card');
+  supplier2card = document.getElementById('supplier2card');
+  supplier3card = document.getElementById('supplier3card');
 
+  supplier1card.onclick = function() {
+    location.href = "supplier1.html";
+  }
+  supplier2card.onclick = function() {
+    location.href = "supplier2.html";
+  }
+  supplier3card.onclick = function() {
+    location.href = "supplier3.html";
+  }
 }
